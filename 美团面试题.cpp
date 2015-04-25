fd791f085b1064db7e8f32c1ff83d598d070e313
easy66 <easy66@live.com>
2015-04-25 20:36:54 +0800

    Update 美团面试题.cpp

--- 美团面试题.cpp
+++ 美团面试题.cpp
@@ -22,43 +22,43 @@ http://www.iteye.com/topic/1134016
 */
 
 int N = 4;
-	int * arr = new int [N * N];
-	int * newArr = new int [5 * N * 5 * N];
-	for (int i = 0; i < N * N; i++)
-		arr[i] = i + 1;
-	memset(newArr, -1, sizeof(int) * 5 * N * 5 * N);
+int * arr = new int [N * N];
+int * newArr = new int [5 * N * 5 * N];
+for (int i = 0; i < N * N; i++)
+	arr[i] = i + 1;
+memset(newArr, -1, sizeof(int) * 5 * N * 5 * N);
 
-	Mat4 mat = {// 绕 z 轴逆时针旋转 π/4 的矩阵
-		cosf(-M_PI / 4), sinf(-M_PI / 4), 0.f, 0.f,
-		-sinf(-M_PI / 4), cosf(-M_PI / 4), 0.f, 0.f,
-		0.f,              0.f,             1.f, 0.f,
-		0.f,              0.f,             0.f, 1.f
-	};
+Mat4 mat = {// 绕 z 轴逆时针旋转 π/4 的矩阵
+	cosf(-M_PI / 4), sinf(-M_PI / 4), 0.f, 0.f,
+	-sinf(-M_PI / 4), cosf(-M_PI / 4), 0.f, 0.f,
+	0.f,              0.f,             1.f, 0.f,
+	0.f,              0.f,             0.f, 1.f
+};
 
-	for (int i = 0; i < N * N; i++)
+for (int i = 0; i < N * N; i++)
+{
+	float x = i % N, y = -float(i / N);// 偷懒,直接设定锚点，不再乘一个平移矩阵了
+	Vec3 vec(x - (N - 1) / 2.f, y + (N - 1) / 2.f, 0);
+	vec.x *= 2; vec.y *= 2;// 因为坐标为整数但矩阵为浮点数，所以让它变大减小误差
+	mat.transformVector(&vec);
+	int newX = round(vec.x) + N;
+	int newY = round(vec.y) + N;
+	newArr[(5 * N - newY - 1) * 5 * N + newX] = arr [i];
+}
+
+for (int y = 0; y < 5 * N; y++)
+{
+	std::string str;
+	for (int x = 0; x < 5 * N; x++)
 	{
-		float x = i % N, y = -float(i / N);// 偷懒,直接设定锚点，不再乘一个平移矩阵了
-		Vec3 vec(x - (N - 1) / 2.f, y + (N - 1) / 2.f, 0);
-		vec.x *= 2; vec.y *= 2;// 因为坐标为整数但矩阵为浮点数，所以让它变大减小误差
-		mat.transformVector(&vec);
-		int newX = round(vec.x) + N;
-		int newY = round(vec.y) + N;
-		newArr[(5 * N - newY - 1) * 5 * N + newX] = arr [i];
-	}
-
-	for (int y = 0; y < 5 * N; y++)
-	{
-		std::string str;
-		for (int x = 0; x < 5 * N; x++)
+		if (newArr[x + y * 5 * N] != -1)
 		{
-			if (newArr[x + y * 5 * N] != -1)
-			{
-				str += std::to_string(newArr[x + y * 5 * N]) + " ";
-			}
+			str += std::to_string(newArr[x + y * 5 * N]) + " ";
 		}
-		if(!str.empty())
-			CCLog(str.c_str());
 	}
-	
-	delete [] arr;
-	delete [] newArr;
+	if(!str.empty())
+		CCLog(str.c_str());
+}
+
+delete [] arr;
+delete [] newArr;
